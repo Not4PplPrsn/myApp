874c3fc895abfa9c276de024de8fdf37b30f2acd
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-10-29 10:39:39 +0200

    Added nw image for the other pages an the rest of the document.

--- app/6-weeks/Child-Minding.tsx
+++ app/6-weeks/Child-Minding.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+import { ImageBackground } from 'expo-image';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function ChildMinding() {
   return (
