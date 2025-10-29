75d989bf1c32cc58da3318547028bba90f036a68
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-10-29 23:13:58 +0200

    Added new imports. Made nave button for each course. Styled each button Added background image for the repsective pages.

--- app/(tabs)/Course.tsx
+++ app/(tabs)/Course.tsx
@@ -1,17 +1,153 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link } from 'expo-router';
-import { ImageBackground } from 'expo-image';
+import { StyleSheet, Text, View , Button, TouchableNativeFeedback,ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { ImageBackground } from 'expo-image';
+import { useRouter } from 'expo-router';
+import { use } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
-export default function Courses() {
+export default function Information() {
   return (
+   <ScrollView
+  
+        snapToAlignment= {'center'}
+      showsHorizontalScrollIndicator={false}
+      style={{ flex: 1 }}
+
+  >
+    <ImageBackground
+    source={require('../../assets/images/Teaching.jpg')}
+    >
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient
+                    colors={['#0a8a30ff', '#043b04ff']}
+              style={StyleSheet.absoluteFill}
+              start={{x:1, y:0.2}}
+              end={{x:0.5, y:1}}
+      
+      >
+      <Text style={styles.header1}>Special</Text>
+      <Text style={styles.text}>
+        Costumers who enrol in multiple courses at Empowering the Nation can receive discounts based on the number of courses they choose. 
+
+If a customer selects one course, no discount is given. Choosing two courses provides a 5% discount, while three courses qualifies for a 10% discount. 
+
+Costumers who select more then three courses enjoy the maximum discount of 15%.
+
+      </Text>
+      <Text style={styles. header1}>Some Food for thought</Text>
+      <Text style={styles.text}>
+        Costumers who enrol in multiple courses at Empowering the Nation can receive discounts based on the number of courses they choose. 
+
+If a customer selects one course, no discount is given. Choosing two courses provides a 5% discount, while three courses qualifies for a 10% discount. 
+
+Costumers who select more then three courses enjoy the maximum discount of 15%.
+      </Text>
+      </LinearGradient>
+    </View></ImageBackground>
+
+
+    <View>  
+      <LinksToTheCourses/>
     </View>
+      <StatusBar style="auto" />
+
+    </ScrollView>
+
+    
+  );
+}
+
+
+
+function LinksToTheCourses(){
+  const router = useRouter();
+  return(
+              <ImageBackground
+    source={require('../../assets/images/6MonthCourses.jpg')}
+    >
+
+    <View>
+
+      <Text> Courses</Text>
+
+      <View style= {{alignItems: 'center', }}>
+        <Text style={styles.header2}>Six Months</Text>
+
+
+          <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#0d50e2ff',true)}
+          onPress={() => router.push('/6-month/FirstAid')}>
+            <View style= {{ borderRadius: 20, width: 190, padding: 6, backgroundColor: "#1143a1e0", margin: 22}}>
+              <Text style={{fontSize: 25, flexDirection: 'row', margin: 10, color: '#fff'}}>First Aid</Text>
+            </View>
+          </TouchableNativeFeedback>
+
+          <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#0e5e2fff',true)}
+          onPress={() => router.push('/6-month/LandScaping')}
+          >
+            <View style= {{ borderRadius: 20, width: 190, padding: 6, backgroundColor: "#219146d7" , margin: 22}}>
+              <Text style={{fontSize: 25, flexDirection: 'row', margin: 10, color: '#fff'}}>Land Scaping </Text>
+            </View>
+          </TouchableNativeFeedback>
+          
+          <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#76e7cfff',true)}
+          onPress= { () => router.push('/6-month/LifeSkills')}>
+            <View style= {{ borderRadius: 20, width: 190, padding: 6, backgroundColor: "#d4259ad0" , margin: 22}}>
+              <Text style={{fontSize: 25, flexDirection: 'row', margin: 10, color: '#fff'}}>Life Skills</Text>
+            </View>
+          </TouchableNativeFeedback>
+          
+
+
+          <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#de61e2ff',true)}
+          onPress={() => router.push('/6-month/Sewing')}>
+            <View style= {{ borderRadius: 20, width: 190, padding: 6, backgroundColor: "#e99e6cf1" , margin: 22}}>
+              <Text style={{fontSize: 25, flexDirection: 'row', margin: 10, color: '#fff'}}>Sewing</Text>
+            </View>
+          </TouchableNativeFeedback>
+
+      </View>
+
+        
+      <View style= {{alignItems: 'center', }}>
+          <Text style={ styles.header2}>Six Weeks</Text>
+        
+          <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#95e5f3ff',true)}
+          onPress={() => router.push('/6-weeks/Child-Minding')}>
+            <View style= {{ borderRadius: 20, width: 190, padding: 6, backgroundColor: "#c4ec10e3" , margin: 22}}>
+              < Text style={{fontSize: 25, flexDirection: 'row', margin: 10, color: '#fff'}}>Child Minding</Text>
+            </View>
+          </TouchableNativeFeedback>
+
+
+          <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#db541eff',true)}
+          onPress={() => router.push('/6-weeks/Cooking')}>
+            <View style= {{ borderRadius: 20, width: 190, padding: 6, backgroundColor: "#d63722ec" , margin: 22}}>
+              <Text style={{fontSize: 25, flexDirection: 'row', margin: 10, color: '#fff'}} >Cooking</Text>
+            </View>
+          </TouchableNativeFeedback>
+
+
+          <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#58ca7aff',true)}
+          onPress={() => router.push('/6-weeks/Gardening')}
+          >
+            <View style= {{ borderRadius: 20, width: 190, padding: 6, backgroundColor: "#0ed33fe5" , margin: 22}}>
+              <Text style={{fontSize: 25, flexDirection: 'row', margin: 10, color: '#fff'}}>Gardening</Text>
+            </View>
+          </TouchableNativeFeedback>
+
+
+      </View>
+
+    </View></ImageBackground>
   );
 }
 
@@ -21,15 +157,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
     alignContent: 'center',
-    height: 340,
-    width: 320,
+    height: 540,
+    width: 340,
     borderWidth: 0.01,
     alignSelf: 'center',
     lineHeight: 5, 
     margin: 199.75,
     padding: 10,
     flex: 1,
-    borderRadius: 20
+    borderRadius: 1000,
+    
 
   },
   text: {
@@ -39,7 +176,8 @@ const styles = StyleSheet.create({
         padding: 4,
         color: '#FFFF', 
         marginStart: 20,
-        margin:5
+        margin:5,
+        lineHeight: 15.9
 
   },
    background: {
@@ -54,9 +192,15 @@ const styles = StyleSheet.create({
 
  },
   header2: {
-  color: '#ffff',
-  fontSize: 16,
+  color: '#349441ff',
+  fontSize: 24,
   fontStyle: 'italic',
+  fontWeight: '300',
+  letterSpacing: 11,
+  backgroundColor: "#ffff",
+  marginTop: 35
+  
+  
   
 
  },
@@ -85,7 +229,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
     alignContent: 'center',
-    height: 650,
+    height: 350,
     width: 320,
     borderWidth: 0.01,
     alignSelf: 'center',
