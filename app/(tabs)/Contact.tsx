dc3850515237d7b55b3ca4f86dcfdda827815753
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-10-30 19:58:35 +0200

    Added new import for the use of the website , Added scroll view in the main page then added the new content of the contacts screen like phone numbers, email offic address and then venue address for the training of customers, Made the images pressable so that they navigate to the google maps page for the address. Also gave the image caption underneath them.

--- app/(tabs)/Contact.tsx
+++ app/(tabs)/Contact.tsx
@@ -1,25 +1,132 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { ImageBackground } from 'expo-image';
+import { StyleSheet, Text, View, ScrollView, Image, ImageBackground, Linking, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import {WebView} from 'react-native-webview';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import MapView,{Marker} from 'react-native-maps'
+import { PROVIDER_GOOGLE } from 'react-native-maps';
+
 
 
 export default function Contacts() {
   return (
+    <ScrollView
+            
+      snapToAlignment= {'center'}
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+      style={{ flex: 1 }}
+
+    >      
+  <ImageBackground
+     source={require('../../assets/images/Contacts.jpg')}
+     style= {styles.background}
+    
+  >
+
+    <View>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+            <LinearGradient
+              colors={['#0a8a30ff', '#043b04ff']}
+     style={[StyleSheet.absoluteFill, { borderRadius: 20 }]}
+              start={{x:1, y:0.2}}
+              end={{x:0.5, y:1}}
+
+            />
+      
+      <Text style= { styles.header1}> Contacts</Text>
+      <Text style= {styles.header2}
+      onPress={()=>Linking.openURL('')}> <Ionicons name='mail-open-outline'  size={25}  ></Ionicons>empoweringthenation@gmail.com</Text>      
+      <Text style={styles.header2}> <Ionicons name='call-outline' size={20}/>0860 567 861</Text>
+      <Text style={styles.header2}onPress={()=>Linking.openURL('https://maps.app.goo.gl/DUxdu1gCozZXsrFN6')}> <Ionicons name='locate-outline' size={20}/>23  Apple Street</Text>
+
+
+      <Text style={ styles.header1} > Venues</Text>
+      <Text style={styles.header2} > <Ionicons name='locate-outline' size={20}/>Shepstone Gardens</Text>
+      <Text style={styles.header2} > <Ionicons name='locate-outline' size={20}/>Living Room  jozi</Text>
+      <Text style={styles.header2}> <Ionicons name='locate-outline' size={20}/>Thaba Eco Hotel</Text>
+
+
+
     </View>
+
+      
+      <View style= { styles.mapsScreens}>
+        <TouchableOpacity
+        activeOpacity={1.7} 
+        onPress={()=>Linking.openURL('https://maps.app.goo.gl/zQHoFp917sPyGoKv7')}
+        >
+      <Image
+      source={require('../../assets/images/DOT.jpg')}
+      style={styles.images}
+      
+      /></TouchableOpacity>
+      <Text style= {styles.captions}>Shepstone Gardens</Text>
+      </View>
+</View>
+    </ImageBackground>
+    
+    <View>
+      <OtherVenues/>
+    </View>
+<StatusBar style="auto" />
+
+
+
+
+
+
+</ScrollView>
+    
+    
   );
 }
 
 function OtherVenues(){
 
   return(
-    <View>
+
+  <ImageBackground
+   source={require('../../assets/images/Contacts.jpg')}
+   style= {styles.background}
+   
+  >
+    <View >
+      
+      
+      <View style={styles.mapsScreens}>
+      <TouchableOpacity
+      activeOpacity={1.7} 
+      onPress={()=>Linking.openURL('https://maps.app.goo.gl/nFNcgWiYsWVYSkuG6')}
+      >
+
+       <Image
+      source={require('../../assets/images/DOT.jpg')}
+      style={styles.images}/>
+      </TouchableOpacity>
+        <Text style= {styles.captions}>Living Room  jozi</Text>
+      </View>
+
+      
+      <View style= { styles.mapsScreens}>
+    <TouchableOpacity
+    activeOpacity={1.7} 
+    onPress={()=>Linking.openURL('https://maps.app.goo.gl/8vZDUscJRTcUehgJ9')}
+    >
+
+              <Image
+      source={require('../../assets/images/DOT.jpg')}
+      style={styles.images}/>
+    </TouchableOpacity>
+
+       
+       <Text style= {styles.captions}>Thaba Eco Hotel</Text>
+      </View>
 
 
     </View>
+    </ImageBackground>
   );
 }
 
@@ -29,12 +136,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
     alignContent: 'center',
-    height: 340,
-    width: 320,
+    height: 380,
+    width: 360,
     borderWidth: 0.01,
     alignSelf: 'center',
     lineHeight: 5, 
-    margin: 199.75,
+    margin: 50.75,
     padding: 10,
     flex: 1,
     borderRadius: 20
@@ -53,18 +160,23 @@ const styles = StyleSheet.create({
    background: {
     flex: 1,
     resizeMode: 'cover', // or 'contain', 'stretch', etc.
+    height: '100%',
   },
  header1: {
-  color: '#ffff',
+  color: '#6eb918ff',
   fontSize: 30,
   fontStyle: 'italic',
-  padding: 15
+  padding: 15,
+  fontWeight: '500',
+  textTransform: 'uppercase'
 
  },
   header2: {
-  color: '#ffff',
+  color: '#ffffffff',
   fontSize: 16,
   fontStyle: 'italic',
+    textTransform: 'uppercase',
+
   
 
  },
@@ -112,6 +224,40 @@ const styles = StyleSheet.create({
     borderRadius: 12,
     overflow: 'hidden',
   },
+    captions: {
+  color: '#2d8209ff',
+  fontSize: 16,
+  fontStyle: 'italic',
+    textTransform: 'uppercase',
+    backgroundColor: '#eeede0af',
+    width: 200,
+    marginTop: 0.4,
+    borderRadius: 30,
+    fontWeight: '600'
+
+  
+
+ },
+ images:{
+  height: 200,
+  width: 290, 
+  margin: 15,
+  alignContent: 'center',
+  borderRadius: 20
+ },
+ mapsScreens:{
+  alignItems: 'flex-start',
+  alignSelf: 'center',
+  justifyContent: 'flex-start',
+  marginTop:50 
+ }
+
+  
+
+
+
+ 
+
 
 
 });
