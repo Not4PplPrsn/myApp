fc188ef369960b8f31950066e4cbccd25429bbc8
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-10-31 18:18:41 +0200

    Added a new thing if statement that made sure that th same object could not be entered twice in the list

--- functions/DataEntries.ts
+++ functions/DataEntries.ts
@@ -1,4 +1,4 @@
-import { create } from 'zustand';
+import { create } from 'zustand';// This function allow for the add button to work and tranfer th information to the calculate screen 
 
 type CourseEntry = {
   id: string;
@@ -9,17 +9,23 @@ type CourseEntry = {
 
 type EntryStore = {
   entries: CourseEntry[];
-  addEntry: (entry: CourseEntry) => void;
+  addEntry: (entry: CourseEntry) => void;//These are meant to store the item names an attributes 
 };
 
-export const useEntries = create<EntryStore>((set, get) => ({
+export const useEntries = create<EntryStore>((set, get) /*This is how the datat is transfered to the calculate screen  */=> ({
   entries: [],
   addEntry: (entry) => {
     const currentEntries = get().entries;
     if (currentEntries.length >= 6) {
       console.warn('Maximum of 6 courses reached');
-      return;
+      return;/*Limits the user from entering more than six items  */
     }
-    set({ entries: [...currentEntries, entry] });
+      const alreadyExists = currentEntries.some(e => e.id === entry.id);
+  if (alreadyExists) {
+    console.warn(`Course with ID ${entry.id} already exists`);
+    return;
+  }
+
+    set({ entries: [...currentEntries, entry] })/*This is the package for he next screen  */;
   },
 }));
