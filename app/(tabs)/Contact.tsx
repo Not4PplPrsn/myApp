d6bae4bfc0441142a857492e2fc1a84caada1e26
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-11-06 04:12:48 +0200

    Added some new stuff for the thing

--- app/(tabs)/Contact.tsx
+++ app/(tabs)/Contact.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { Image, ImageBackground, Linking, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 
-
 export default function Contacts() {
   return (
     <ScrollView
